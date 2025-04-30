90a46fd588b7c99561607c83ab549f62dfa387ae
oxilunna <lunnasousafe@gmail.com>
2025-04-30 09:21:17 -0300

    Update teste_listarCliente.php
    correção ortográfica

--- renascer/codigo/tests/teste_listarCliente.php
+++ renascer/codigo/tests/teste_listarCliente.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "../conexao.php"; //conectar com o BD
-require_once "../funcoes.php"; //conectar com o arquivo funôes
+require_once "../funcoes.php"; //conectar com o arquivo funções
 
 echo "<pre>"; // formatar texto para quebrar linhas
 print_r(listarClientes($conexao)); //Chama a função listarClientes. Print_r → Mostra o conteúdo do array de forma estruturada (fica fácil de entender o resultado). //Se for só texto → print. Se for array ou objeto → print_r
