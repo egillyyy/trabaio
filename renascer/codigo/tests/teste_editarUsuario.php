5e6b5252a5eec8f5f665ec30021cbf7279eb007e
oxilunna <lunnasousafe@gmail.com>
2025-05-08 14:20:24 -0300

    refiz editar usuário

--- renascer/codigo/tests/teste_editarUsuario.php
+++ renascer/codigo/tests/teste_editarUsuario.php
@@ -3,13 +3,14 @@
     require_once "../funcoes.php";
     
 
-    $email = "fernando@gmail.com";
+    $email = "Rodandinho@gmail.com";
     $senha_hash = "4567";
     $tipo = "1";
     $nome = "aaaa";
     $telefone = "62 99009571";
     $cpf = "000.467.666-00";
+    $idusuario = "1"
 
-    salvarUsuario($conexao, $email, $senha_hash, $tipo, $nome, $telefone, $cpf);
+    salvarUsuario($conexao, $email, $senha_hash, $tipo, $nome, $telefone, $cpf, $idusuario);
 
 ?>
