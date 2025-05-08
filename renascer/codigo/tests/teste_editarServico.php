fbc3d78009d0782b48572d1870fcb36b462aa21e
oxilunna <lunnasousafe@gmail.com>
2025-05-08 14:49:21 -0300

    corrigi

--- renascer/codigo/tests/teste_editarServico.php
+++ renascer/codigo/tests/teste_editarServico.php
@@ -4,9 +4,10 @@
     
     $preco_servico = "40.00";
     $tipo_servico = "Cabelo";
+    $idservico = "1";
    
 
-    editarServico($conexao, $preco_servico, $tipo_servico);
+    editarServico($conexao, $preco_servico, $tipo_servico, $idservico);
 ?>
 
 
