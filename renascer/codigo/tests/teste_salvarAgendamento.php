0c75666788824e9dc896219e4506c4180dbc3c42
oxiNina <marinalealpedroso@gmail.com>
2025-05-12 14:36:11 -0300

    SALVARAGENDAMENTOO CERTO
    FINALMENTE CONSEGUI SALVAARRR

--- renascer/codigo/tests/teste_salvarAgendamento.php
+++ renascer/codigo/tests/teste_salvarAgendamento.php
@@ -4,9 +4,9 @@
 
 $data = "2025-05-12";
 $horario = "14:30:00";
-$tb_taxa_idtaxa = 1; 
-$tb_servico_id_servico = 1;
+$tb_taxa_idtaxa = 2; 
+$tb_servico_id_servico = 2;
 
-    salvarAgendamento($conexao, $data, $horario, $tb_cliente_idcliente, $tb_taxa_idtaxa, $tb_servico_id_servico)
+    salvarAgendamento($conexao, $data, $horario, $tb_taxa_idtaxa, $tb_servico_id_servico)
 
 ?>
