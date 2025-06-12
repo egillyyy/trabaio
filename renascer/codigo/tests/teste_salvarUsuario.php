42ecb09ba9a4a998a69aa29c5f0d273ae690716d
oxiNina <marinalealpedroso@gmail.com>
2025-06-12 13:39:19 -0300

    coloquei + coisos no banco p testar

--- renascer/codigo/tests/teste_salvarUsuario.php
+++ renascer/codigo/tests/teste_salvarUsuario.php
@@ -3,11 +3,52 @@
     require_once "../funcoes.php";
 
 
-    $email = "teste2@gmail.com";
-    $senha_hash = "321";
+    <?php
+    require_once "../conexao.php";
+    require_once "../funcoes.php";
+
+
+    // Usuário 1
+    $email = "ana.silva@gmail.com";
+    $senha_hash = "senha123";
     $tipo = "2";
-    $nome = "bbbb";
-    $telefone = "62 99008888";
+    $nome = "Ana Silva";
+    $telefone = "11 999999999";
+    salvarUsuario($conexao, $email, $senha_hash, $tipo, $nome, $telefone);
+
+    // Usuário 2
+    $email = "lucas.moura@gmail.com";
+    $senha_hash = "abc321";
+    $tipo = "1";
+    $nome = "Lucas Moura";
+    $telefone = "21 988888888";
+    salvarUsuario($conexao, $email, $senha_hash, $tipo, $nome, $telefone);
+
+    // Usuário 3
+    $email = "carla.oliveira@gmail.com";
+    $senha_hash = "123456";
+    $tipo = "2";
+    $nome = "Carla Oliveira";
+    $telefone = "31 977777777";
+    salvarUsuario($conexao, $email, $senha_hash, $tipo, $nome, $telefone);
+
+    // Usuário 4
+    $email = "ricardo.souza@gmail.com";
+    $senha_hash = "teste321";
+    $tipo = "1";
+    $nome = "Ricardo Souza";
+    $telefone = "41 966666666";
+    salvarUsuario($conexao, $email, $senha_hash, $tipo, $nome, $telefone);
+
+    // Usuário 5
+    $email = "maria.lima@gmail.com";
+    $senha_hash = "maria321";
+    $tipo = "2";
+    $nome = "Maria Lima";
+    $telefone = "51 955555555";
+    salvarUsuario($conexao, $email, $senha_hash, $tipo, $nome, $telefone);
+
+?>
 
     salvarUsuario($conexao, $email, $senha_hash, $tipo, $nome, $telefone);
 ?>
