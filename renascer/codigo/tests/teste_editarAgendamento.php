4807d849670355b5445894894cee92342aba7f96
oxiNina <marinalealpedroso@gmail.com>
2025-06-16 14:02:38 -0300

    EDITAR AGEBDAMENTO FUNCIONANDOOOOOOOOOO

--- renascer/codigo/tests/teste_editarAgendamento.php
+++ renascer/codigo/tests/teste_editarAgendamento.php
@@ -3,10 +3,10 @@ require_once "../conexao.php";
 require_once "../funcoes.php";
 
 $data = "2025-06-15";
-$horario = "15:15:00";
-$tb_taxa_idtaxa = 1;            // ID da taxa existente na tabela tb_taxa
+$horario = "15:10:00";
 $tb_servico_id_servico = 1;     // ID do serviço existente
-$idagendamento = 8;             // ID do agendamento que você deseja editar
+$tb_usuario_idusuario_cliente = 1;
+$tb_usuario_idusuario_funcionario = 1;
+$idagendamento = 1;             // ID do agendamento que você deseja editar
 
-editarAgendamento($conexao, $data, $horario, $tb_taxa_idtaxa, $tb_servico_id_servico, $tb_pagamento_idpagamento, $idagendamento);
-
+editarAgendamento($conexao, $data, $horario, $tb_servico_id_servico, $tb_usuario_idusuario_cliente, $tb_usuario_idusuario_funcionario, $idagendamento);
