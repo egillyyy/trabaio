ab52e74a21840e1123b0db7458eddb6f8e4eb6bf
oxiNina <marinalealpedroso@gmail.com>
2025-08-28 16:17:12 -0300

    SALVAR SERVICO E FORM FUNCIONOUUUUUUUUUUUUUUUUUUU

--- renascer/public/formServico.php
+++ renascer/public/formServico.php
@@ -6,6 +6,27 @@
     <title>Formulário de Serviço</title>
 </head>
 <body>
-    
+    <?php
+    require_once "../codigo/conexao.php";
+    require_once "../codigo/funcoes.php";
+
+    $lista_servicos = listarServico($conexao);
+    ?>
+
+    <h1>Selecionar Serviço</h1>
+
+    <form action="salvarServico.php" method="get">
+        <select name="idservico">
+            <?php foreach ($lista_servicos as $servico) { 
+                $id = $servico['idservico'];
+                $tipo = $servico['tipo_servico'];
+                $preco = $servico['preco_servico'];
+            ?>
+                <option value="<?= $id ?>"><?= $tipo ?> - R$ <?= $preco ?></option>
+            <?php } ?>
+        </select>
+    <br><br>
+    <button type="submit">Salvar</button>
+</form>
 </body>
 </html>
--- renascer/public/salvarServico.php
+++ renascer/public/salvarServico.php
@@ -6,7 +6,35 @@
     <title>Salbar Serviço</title>
 </head>
 <body>
-    <h1>Salvar Serviço</h1>
+<?php
+    require_once "../codigo/conexao.php";
+    require_once "../codigo/funcoes.php";
+
+// Pega o serviço selecionado pelo usuário
+    $idservico = $_GET['idservico'];
+
+// Busca o serviço no banco 
+    $servico = pesquisarServicoId($conexao, $idservico);
+
+    if ($servico) {
+        $preco_servico = $servico['preco_servico'];
+        $tipo_servico  = $servico['tipo_servico'];
+
+    // Salva o serviço novamente no banco (ou registra de acordo com sua lógica)
+        $funcionou = salvarServico($conexao, $preco_servico, $tipo_servico);
+
+        if ($funcionou) {
+            echo "Serviço $tipo_servico salvo com sucesso";
+            echo "<a href='listar_servico.php'>Voltar para lista de serviços</a>";
+        } else {
+            echo "Erro ao salvar o serviço.";
+        }
+    } else {
+        echo " Serviço não encontrado";
+    }
+
+    ?>
+
     
 </body>
 </html>
