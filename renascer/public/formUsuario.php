c18ab00ffb7761c026f9a663f496e462b7fefd06
egillyyy <egillyandrade@gmail.com>
2025-08-28 09:48:55 -0300

    Conexão

--- renascer/public/formUsuario.php
+++ renascer/public/formUsuario.php
@@ -1,3 +1,11 @@
+<?php
+    require_once "/../codigo/conexao.php";
+    require_once "/../codigo/funcoes.php";
+
+   salvarUsuario($conexao, $email, $senha, $tipo, $nome, $telefone);
+
+?> 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,19 +15,26 @@
 </head>
 <body>
     <form action="" method="post">
-        <h1>Acessar sistema</h1>
+        <h1>Criar conta</h1>
 
-        Login: <br>
+        Nome: <br>
         <input type="text" name="login"> <br><br>
+
+        Telefone: <br>
+        <input type="text" name="telefone"> <br><br>
+
+        Email: <br>
+        <input type="password" name="email"> <br><br>
 
         Senha: <br>
         <input type="password" name="senha"> <br><br>
 
-        <input type="submit" value="Acessar">
+        <input type="submit" value="logar">
     </form>
     <br>
     <!-- <a href="formUsuario.html">Cadastrar Usuário</a> -->
-    <button type="submit"> <a href="formUsuario.html">Cadastrar Usuário</a></button>
+    <button type="submit"> <a href="index.php">Logar</a></button>
+
     
 </body>
 </html>
