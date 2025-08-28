79d9248f24a7d81f85788047e5da9c58e2f96591
oxiNina <marinalealpedroso@gmail.com>
2025-08-28 15:31:43 -0300

    colocando os salvar

--- renascer/public/index.php
+++ renascer/public/index.php
@@ -11,5 +11,9 @@
     <a href="formServico.php">Formulário de Serviço</a> <br><br>
     <a href="formUsuario.php">Formulário de Usuário</a> <br><br>
     <a href="home.html">Ir pra Home</a>
+    <a href="listarAgendamentos.php">listar agendamentos</a> <br> <br>
+    <a href="listarPagamentos.php">listar pagamentos</a> <br> <br>
+    <a href="listarServicos.php">listar serviços</a> <br> <br>
+    <a href="listarUsuarios.php">listar usuarios</a> <br> <br>
 </body>
 </html>
