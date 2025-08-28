d0db93f92b4974c4edfe85f277d3be039aaa049d
egillyyy <egillyandrade@gmail.com>
2025-08-28 15:49:58 -0300

    pesquisarPagamento

---
+++ renascer/codigo/tests/teste_pesquisarPagamentoID.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "../conexao.php";
+require_once "../funcoes.php";
+
+$idpagamento = "1";
+
+echo "<pre>";
+print_r(pesquisarPagamentoId($conexao, $idpagamento));
+echo "</pre>";
+?>
