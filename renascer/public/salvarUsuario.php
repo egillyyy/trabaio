1990eacdaf4e21698a5d8a73d17a50b2e34642ce
egillyyy <egillyandrade@gmail.com>
2025-09-01 14:08:40 -0300

    TA SALVANDO NO BANCOOOOOO

--- renascer/public/salvarUsuario.php
+++ renascer/public/salvarUsuario.php
@@ -3,16 +3,13 @@ require_once "../codigo/conexao.php";
 require_once "../codigo/funcoes.php";
 
 $id = $_GET['id'];
-$email = $usuario['email'];
-$senha_hash = $usuario['senha'];
-$nome = $usuario['nome'];
-$telefone = $usuario['telefone'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$tipo = $_POST['c'];
+$nome = $_POST['nome'];
+$telefone = $_POST['telefone'];
 
-if ($id == 0) {
 salvarUsuario($conexao, $email, $senha, $tipo, $nome, $telefone);
-} else {
-    editarUsuario($conexao, $email, $senha, $tipo, $nome, $telefone, $idusuario);
-}
 
 header("Location: listarUsuarios.php");
 ?>
