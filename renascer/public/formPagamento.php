36dbec744e174cfb4d3269a660e512607bf41c48
egillyyy <egillyandrade@gmail.com>
2025-09-01 14:24:24 -0300

    Arrumei aqui

--- renascer/public/formPagamento.php
+++ renascer/public/formPagamento.php
@@ -1,32 +1,3 @@
-<?php
-    if (isset($_GET['id'])) {
-        // echo "editar";
-
-        require_once "../codigo/conexao.php";
-        require_once "../codigo/funcoes.php";
-
-        $id = $_GET['id'];
-        
-        $pagamento = salvarPagamento($conexao, $valor, $forma, $descricao, $tb_agendamento_idagendamento);
-        $valor = $pagamento['valor'];
-        $forma = $pagamento['forma'];
-        $descricao = $pagamento['descricao'];
-        $tb_agendamento_idagendamento = $pagamento['tb_agendamento_idagendamento'];
-
-        $botao = "Atualizar";
-    }
-    else {
-        // echo "novo";
-        $id = 0;
-        $valor = "";
-        $forma = "";
-        $descricao = "";
-        $tb_agendamento_idagendamento = "";
-
-        $botao = "Cadastrar";
-    }
-?> 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -52,38 +23,7 @@
         <textarea name="descricao" placeholder="Escreva sua mensagem aqui..."></textarea>
         <br><br>
         <button type="submit"> <a href="index.php">Fazer pagamento</a></button>
-
-
-<!-- 
-        <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Novo Pagamento</title>
-</head>
-<body>
-
-    <h1>Cadastrar Pagamento</h1>
-
-    <form action="SalvarPagamento.php" method="post">
-        Valor:
-        <input type="text" name="valor" required><br><br>
-
-        Forma:
-        <input type="text" name="forma" required><br><br>
-
-        Descrição:
-        <input type="text" name="descricao"><br><br>
-
-        ID Agendamento: 
-        <input type="number" name="idagendamento" required><br><br>
-
-        <input type="submit" value="Salvar">
-    </form> -->
-
-</body>
-</html>
+        
     </form>
 </body>
 </html>
--- renascer/public/salvarPagamento.php
+++ renascer/public/salvarPagamento.php
@@ -2,10 +2,10 @@
 require_once "../codigo/conexao.php";
 require_once "../codigo/funcoes.php";
 
-$valor = $_POST["valor"];
-$forma = $_POST["forma"];
-$descricao = $_POST["descricao"];
-$idagendamento = $_POST["idagendamento"];
+$valor = $_POST['valor'];
+$forma = $_POST['forma'];
+$descricao = $_POST['descricao'];
+$idagendamento = $_POST['idagendamento'];
 
 // Função que você deve ter no funcoes.php
 salvarPagamento($conexao, $valor, $forma, $descricao, $idagendamento);
