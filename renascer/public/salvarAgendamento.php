c58e00a52932c0d0505f55a8f7f2c59331e859b4
egillyyy <egillyandrade@gmail.com>
2025-09-25 10:50:01 -0300

    mudei aqui mas tem que terminar

--- renascer/public/salvarAgendamento.php
+++ renascer/public/salvarAgendamento.php
@@ -7,7 +7,7 @@ $horario = $_POST['horario'];
 $idservico = $_POST['idservico'];
 $idcliente = $_POST['idcliente'];
 
-salvarAgendamento ($conexao, $data, $horario, $tb_servico_id_servico, $tb_usuario_idusuario_cliente);
+salvarAgendamento ($conexao, $data, $horario, $tb_servico_id_servico, $tb_usuario_idusuario);
 
 header("Location: listarAgendamento.php");
 ?>
