0816298ea0fde1c5f8a678351757c3e7d78f4584
oxiNina <marinalealpedroso@gmail.com>
2025-09-29 14:18:56 -0300

    aa que dor de cabeçaa

--- renascer/codigo/tests/teste_editarAgendamento.php
+++ renascer/codigo/tests/teste_editarAgendamento.php
@@ -6,7 +6,11 @@ $data = "2025-06-15";
 $horario = "15:10:00";
 $tb_servico_id_servico = 1;     // ID do serviço existente
 $tb_usuario_idusuario_cliente = 1;
-$tb_usuario_idusuario_funcionario = 1;
 $idagendamento = 1;             // ID do agendamento que você deseja editar
 
-editarAgendamento($conexao, $data, $horario, $tb_servico_id_servico, $tb_usuario_idusuario_cliente, $tb_usuario_idusuario_funcionario, $idagendamento);
+if (editarAgendamento($conexao, $data, $horario, $tb_servico_id_servico, $tb_usuario_idusuario_cliente, $idagendamento)) {
+    echo "Agendamento atualizado com sucesso!";
+} else {
+    echo "Erro ao atualizar o agendamento.";
+}
+?>
