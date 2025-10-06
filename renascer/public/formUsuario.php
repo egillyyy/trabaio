c0889bcaa7cf07c5f063d16abda47d2635569490
oxiNina <marinalealpedroso@gmail.com>
2025-10-06 14:44:15 -0300

    usuario css

--- renascer/public/formUsuario.php
+++ renascer/public/formUsuario.php
@@ -5,11 +5,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Criar Conta</title>
-  <!-- Link do Bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body class="fundo-verde">
+    <h1 class="letra-dourada">Ateliê Renascer</h1>
 
   <div class="container mt-5">
     <div class="card mx-auto" style="max-width: 400px;">
@@ -18,31 +18,26 @@
 
         <form action="salvarUsuario.php" method="post">
           
-          <!-- Nome -->
           <div class="mb-3">
             <label for="nome" class="form-label">Nome</label>
             <input type="text" class="form-control" id="nome" name="nome" required>
           </div>
 
-          <!-- Telefone -->
           <div class="mb-3">
             <label for="telefone" class="form-label">Telefone</label>
             <input type="text" class="form-control" id="telefone" name="telefone" required>
           </div>
 
-          <!-- Email -->
           <div class="mb-3">
             <label for="email" class="form-label">E-mail</label>
             <input type="email" class="form-control" id="email" name="email" required>
           </div>
 
-          <!-- Senha -->
           <div class="mb-3">
             <label for="senha" class="form-label">Senha</label>
             <input type="password" class="form-control" id="senha" name="senha" required>
           </div>
 
-          <!-- Tipo de usuário -->
           <input type="hidden" name="tipo" value="c">
 
           <!-- Botão Criar conta -->
