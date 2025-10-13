94393ad4018b1ebaf4e97b0d956d6bbad44b648a
oxiNina <marinalealpedroso@gmail.com>
2025-10-13 13:30:50 -0300

    css

--- renascer/public/listarServicos.php
+++ renascer/public/listarServicos.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/style.css">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <title>Document</title>
 </head>
-<body>
+<body class="fundo-verde">
     <h1>Listar Serviços</h1>
 
      <?php
