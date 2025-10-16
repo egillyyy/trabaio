064c752a255c2c1589e958396951b1cf23985de3
egillyyy <egillyandrade@gmail.com>
2025-10-16 10:50:24 -0300

    eu tentei

--- renascer/codigo/tests/teste_pesquisarUsuarioNome.php
+++ renascer/codigo/tests/teste_pesquisarUsuarioNome.php
@@ -2,10 +2,8 @@
 require_once "../conexao.php";
 require_once "../funcoes.php";
 
-
 $nome_pesquisado = "a";
 $usuarios = pesquisarUsuarioNome($conexao, $nome_pesquisado);
-
 
 foreach ($usuarios as $usuario) {
    echo $usuario["nome"] . "<br>";
