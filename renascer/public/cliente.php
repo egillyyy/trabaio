8c17023e58fc1ccf06179642857cfb86c6547b6a
oxiNina <marinalealpedroso@gmail.com>
2025-10-16 10:34:10 -0300

    o egilly, pux auisso aq

---
+++ renascer/public/cliente.php
@@ -0,0 +1,12 @@
+<!-- EGILLY E MARINA ESSA PAGINA SELECIONA O CLIENTE -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selecionar Cliente</title>
+ </head>
+ <body>
+    
+ </body>
+ </html>
