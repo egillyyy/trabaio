b5f67e51bd2fcd6c46a4bdf110888bc2c9cab299
egillyyy <egillyandrade@gmail.com>
2025-10-30 08:52:07 -0300

    arrumaei

--- renascer/public/salvarServico.php
+++ renascer/public/salvarServico.php
@@ -1,19 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Salbar Serviço</title>
-</head>
-<body>
 <?php
     require_once "../codigo/conexao.php";
     require_once "../codigo/funcoes.php";
 
     $id = $_GET['id'];
     $preco_servico = $_POST['preco_servico'];
-    $tipo_servico = $_POST['tipo_servico'];
+    // $tipo_servico = $_POST['tipo_servico'];
     $descricao_servico = $_POST['descricao_servico'];
+    $idservico = $_POST['idservico'];
+
+    $servico = pesquisarServicoId($conexao, $idservico);
+    $tipo_servico = $servico['tipo_servico'];
 
     $nome_arquivo = $_FILES['foto']['name'];
     $caminho_temporario = $_FILES['foto']['tmp_name'];
@@ -37,32 +33,8 @@
         editarServico($conexao, $preco_servico, $tipo_servico, $descricao_servico, $id);    
     }
 
-    header("Location: listarServico.php");
-
-// Pega o serviço selecionado pelo usuário
-    $idservico = $_GET['idservico'];
-
-// Busca o serviço no banco 
-    $servico = pesquisarServicoId($conexao, $idservico);
-
-    if ($servico) {
-        $preco_servico = $servico['preco_servico'];
-        $tipo_servico  = $servico['tipo_servico'];
-
-    // Salva o serviço novamente no banco 
-        $funcionou = salvarServico($conexao, $preco_servico, $tipo_servico, $descricao_servico, $foto);
-
-        if ($funcionou) {
-            echo "Serviço $tipo_servico salvo com sucesso";
-            echo "<a href='listar_servico.php'>Voltar para lista de serviços</a>";
-        } else {
-            echo "Erro ao salvar o serviço.";
-        }
-    } else {
-        echo " Serviço não encontrado";
-    }
-
-    ?>
+    header("Location: listarServicos.php");
+?>
 
     
 </body>
