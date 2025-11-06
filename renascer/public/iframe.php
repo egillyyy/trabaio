1d399edba5504bc537a6f9c830ccd7d02108b2f5
egillyyy <egillyandrade@gmail.com>
2025-11-06 14:06:39 -0300

    exploda

---
+++ renascer/public/iframe.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ateliê Renascer</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="fundo-verde">
+
+  <main class="conteudo">
+    <iframe src="home.php" name="conteudo" class="conteudo-frame"></iframe>
+  </main>
+
+  <footer class="footer-fixo">
+    <?php include "rodape.html"; ?>
+  </footer>
+
+</body>
+</html>
