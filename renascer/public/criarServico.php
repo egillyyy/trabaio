996ffa5b5b639cab1f48bd531e7e3de07fa06cb3
egillyyy <egillyandrade@gmail.com>
2025-11-10 15:44:13 -0300

    CSS

--- renascer/public/criarServico.php
+++ renascer/public/criarServico.php
@@ -1,9 +1,7 @@
 <?php
     require_once "../codigo/verificarLogado.php";
     $tipo = $_SESSION['tipo'];
-?>
 
-<?php
     require_once "../codigo/conexao.php";
     require_once "../codigo/funcoes.php";
 
@@ -36,16 +34,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastro de Serviço</title>
 </head>
-<body>
-    <h1>Cadastro de Serviços</h1>
-    <form action="salvarServico.php?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
+<body class="fundo-verde">
+    <h1 class="card-title text-center mb-4" id="tituloagendamento">Cadastro de Serviços</h1>
+    <form action="salvarServico.php?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data" class="card card-agendamento p-4 shadow">
         
-        Preço do Serviço: <br>
-        <input type="text" name="preco_servico" value="<?php echo $preco_servico; ?>"> <br><br>
+        <div class="mb-3">
+            <label for="preco_servico" class="form-label">Preço do Serviço:</label>
+            <input type="text" class="form-control" id="preco_servico" name="preco_servico" value="<?php echo $preco_servico; ?>" required>
+        </div>
+        <br><br> 
 
         Tipo do Servico: <br>
             <select class="form-select" id="idservico" name="idservico" required>
@@ -58,8 +61,10 @@
             </select>
         <br><br>
 
-        Descrição do Serviço: <br>
-        <textarea name="descricao_servico" rows="3" placeholder="Escreva algo sobre o serviço..." value="<?php echo $descricao_servico; ?>"></textarea> <br><br>
+        <div class="mb-3">
+            <label for="descricao_servico" class="form-label">Descrição do Serviço:</label>
+            <textarea type="text" class="form-control" id="descricao_servico" name="descricao_servico"  placeholder="Escreva algo sobre o serviço..." value="<?php echo $descricao_servico; ?>" required></textarea> <br><br>
+        </div>
 
         Foto: <br>
         <input type="file" name="foto"> <br><br>
