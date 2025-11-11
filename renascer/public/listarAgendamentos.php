c580795f51d04688aeee1da5ff6c314da2e57948
oxiNina <marinalealpedroso@gmail.com>
2025-11-11 12:55:04 -0300

    os listar arrumado

--- renascer/public/listarAgendamentos.php
+++ renascer/public/listarAgendamentos.php
@@ -8,14 +8,13 @@
 <head>
     <link rel="stylesheet" href="css/style.css">
     <meta charset="UTF-8">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>agendamento</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Listar Agendamentos</title>
 </head>
 <body class="fundo-verde">
 
-    <h1 id="h1arrumado">Lista de Agendamentos</h1>
+    <h1 class="listarU">Lista de Agendamentos</h1>
 
     <?php
     require_once "../codigo/conexao.php";
@@ -27,42 +26,45 @@
         echo "Não existem agendamentos cadastrados.";
     } else {
     ?>
-        <table class="table">
-            <thead>
-                <th scope="col">ID</th>
-                <th scope="col">Data</th>
-                <th scope="col">Horário</th>
-                <th scope="col">Serviço</th>
-                <th scope="col">Cliente</th>
-                <th colspan="2">Ações</th>
-            </tr>
+        <div class="table">
+            <table class="table table-striped table-success">
+                <thead class="text-center">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Horário</th>
+                        <th scope="col">Serviço</th>
+                        <th scope="col">Cliente</th>
+                        <th scope="col" colspan="2">Ações</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <?php
+                    foreach ($lista_agendamento as $agendamento) {
+                        $id = $agendamento['idagendamento'];
+                        $data = $agendamento['data'];
+                        $horario = $agendamento['horario'];
+                        $servico = $agendamento['nome_servico'];
+                        $cliente = $agendamento['nome_cliente'];
 
-            <?php
-            foreach ($lista_agendamento as $agendamento) {
-                $id = $agendamento['idagendamento'];
-                $data = $agendamento['data'];
-                $horario = $agendamento['horario'];
-                $servico = $agendamento['nome_servico'];
-                $cliente = $agendamento['nome_cliente'];
-
-            echo "<tbody>";    
-                echo "<tr>";
-                echo "<td>$id</td>";
-                echo "<td>$data</td>";
-                echo "<td>$horario</td>";
-                echo "<td>$servico</td>";
-                echo "<td>$cliente</td>";
-                echo "<td><a href='../codigo/deletarAgendamento.php?id=$id' onclick=\"return confirm('Tem certeza que deseja excluir este agendamento?')\">Excluir</a></td>";
-                echo "<td><a href='formAgendamento.php?id=$id'>Editar</a></td>";
-                echo "</tr>";
-            echo "</tbody>";
-            }
-            ?>
-        </table>
+                        echo "<tr>";
+                        echo "<td>$id</td>";
+                        echo "<td>$data</td>";
+                        echo "<td>$horario</td>";
+                        echo "<td>$servico</td>";
+                        echo "<td>$cliente</td>";
+                        echo "<td><a href='../codigo/deletarAgendamento.php?id=$id''>Excluir</a></td>";
+                        echo "<td><a href='formAgendamento.php?id=$id'>Editar</a></td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     <?php
     }
     ?>
-  
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
