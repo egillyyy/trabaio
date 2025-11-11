24820d6b3a1389ebe1c7da61794447dee18b7e22
egillyyy <egillyandrade@gmail.com>
2025-11-11 11:34:13 -0300

    Arrumei o tipo da escolha em serviço

--- renascer/public/criarServico.php
+++ renascer/public/criarServico.php
@@ -1,62 +1,79 @@
 <?php
-    require_once "../codigo/verificarLogado.php";
-    $tipo = $_SESSION['tipo'];
+require_once "../codigo/verificarLogado.php";
+$tipo = $_SESSION['tipo'];
 ?>
 
 <?php
-    require_once "../codigo/conexao.php";
-    require_once "../codigo/funcoes.php";
+require_once "../codigo/conexao.php";
+require_once "../codigo/funcoes.php";
 
-    $servicos = listarServico($conexao);
+$servicos = listarServico($conexao);
 
-    if (isset($_GET['id'])) {
-        // echo "editar";
+if (isset($_GET['id'])) {
+    // echo "editar";
 
-        $id = $_GET['id'];
-        
-        $servico = pesquisarServicoId($conexao, $idservico);
-        
-        $servico = pesquisarServicoId($conexao, $idservico);
-        $preco_servico = $servico['preco_servico'];
-        $tipo_servico = $servico['tipo_servico'];
-        $descricao_servico = $servico['descricao_servico'];
+    $id = $_GET['id'];
+    $servico = pesquisarServicoId($conexao, $id);
 
-        $botao = "Atualizar";
-    }
-    else {
-        // echo "novo";
-        $id = 0;
-        $preco_servico = "";
-        $tipo_servico = "";
-        $descricao_servico = "";
+    $preco_servico = $servico['preco_servico'];
+    $tipo_servico = $servico['tipo_servico'];
+    $descricao_servico = $servico['descricao_servico'];
 
-        $botao = "Cadastrar";
-    }
+    $botao = "Atualizar";
+} else {
+    // echo "novo";
+    $id = 0;
+    $preco_servico = "";
+    $tipo_servico = "";
+    $descricao_servico = "";
+
+    $botao = "Cadastrar";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Cadastro de Serviços</h1>
     <form action="salvarServico.php?id=<?php echo $id; ?>" method="post" enctype="multipart/form-data">
-        
+
         Preço do Serviço: <br>
         <input type="text" name="preco_servico" value="<?php echo $preco_servico; ?>"> <br><br>
 
-        Tipo do Servico: <br>
-            <select class="form-select" id="idservico" name="idservico" required>
-                <option value="">Selecione</option>
-                <?php
-                foreach ($servicos as $selecione) {
-                    echo "<option value='" . $selecione['idservico'] . "'>" . $selecione['tipo_servico'] . "</option>";
-                }
-                ?>
-            </select>
-        <br><br>
+        <div class="mb-3">
+            <label class="form-label">Tipo do Servico:</label><br>
+
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="tipo_servico" id="cabelo" value="Cabelo" required>
+                <label class="form-check-label" for="cabelo">Cabelo</label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="tipo_servico" id="penteado" value="Penteado">
+                <label class="form-check-label" for="penteado">Penteado</label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="tipo_servico" id="maquiagem" value="Maquiagem" required>
+                <label class="form-check-label" for="maquiagem">Maquiagem</label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="tipo_servico" id="unha" value="Unha">
+                <label class="form-check-label" for="unha">Unha</label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="tipo_servico" id="especial" value="Especial">
+                <label class="form-check-label" for="especial">Especial</label>
+            </div>
+        </div>
 
         Descrição do Serviço: <br>
         <textarea name="descricao_servico" rows="3" placeholder="Escreva algo sobre o serviço..." value="<?php echo $descricao_servico; ?>"></textarea> <br><br>
@@ -67,4 +84,5 @@
         <input type="submit" value="<?php echo $botao; ?>">
     </form>
 </body>
+
 </html>
--- renascer/public/salvarServico.php
+++ renascer/public/salvarServico.php
@@ -1,44 +1,36 @@
 <?php
-    require_once "../codigo/verificarLogado.php";
-    $tipo = $_SESSION['tipo'];
+require_once "../codigo/verificarLogado.php";
+$tipo = $_SESSION['tipo'];
 
-    require_once "../codigo/conexao.php";
-    require_once "../codigo/funcoes.php";
+require_once "../codigo/conexao.php";
+require_once "../codigo/funcoes.php";
 
-    $id = $_GET['id'];
-    $preco_servico = $_POST['preco_servico'];
-    // $tipo_servico = $_POST['tipo_servico'];
-    $descricao_servico = $_POST['descricao_servico'];
-    $idservico = $_POST['idservico'];
+$id = $_GET['id'];
+$preco_servico = $_POST['preco_servico'];
+$tipo_servico = $_POST['tipo_servico'];  // vem do form (radio)
+$descricao_servico = $_POST['descricao_servico'];
 
-    $nome_arquivo = $_FILES['foto']['name'];
-    $caminho_temporario = $_FILES['foto']['tmp_name'];
-
-    $servico = pesquisarServicoId($conexao, $idservico);
-    $tipo_servico = $servico['tipo_servico'];
-
-    $nome_arquivo = $_FILES['foto']['name'];
-    $caminho_temporario = $_FILES['foto']['tmp_name'];
+$nome_arquivo = $_FILES['foto']['name'];
+$caminho_temporario = $_FILES['foto']['tmp_name'];
 
 
-    //pega a extensão do arquivo
-    $extensao = pathinfo($nome_arquivo, PATHINFO_EXTENSION);
+//pega a extensão do arquivo
+$extensao = pathinfo($nome_arquivo, PATHINFO_EXTENSION);
 
-    //gerar um novo nome para o arquivo
-    $novo_nome = uniqid() . "." . $extensao;
+//gerar um novo nome para o arquivo
+$novo_nome = uniqid() . "." . $extensao;
 
-    //criar um novo caminho para o arquivo
-    // lembre-se de criar a pasta e ajustar as permissões
-    $caminho_destino = "fotos_servico/" . $novo_nome;
+//criar um novo caminho para o arquivo
+// lembre-se de criar a pasta e ajustar as permissões
+$caminho_destino = "fotos_servico/" . $novo_nome;
 
-    // move a foto para o servidor
-    move_uploaded_file($caminho_temporario, $caminho_destino);
+// move a foto para o servidor
+move_uploaded_file($caminho_temporario, $caminho_destino);
 
-    if ($id == 0) {
-        salvarServico($conexao, $preco_servico, $tipo_servico, $descricao_servico, $novo_nome);    
-    } else {
-        editarServico($conexao, $preco_servico, $tipo_servico, $descricao_servico, $id);    
-    }
+if ($id == 0) {
+    salvarServico($conexao, $preco_servico, $tipo_servico, $descricao_servico, $novo_nome);
+} else {
+    editarServico($conexao, $preco_servico, $tipo_servico, $descricao_servico, $id);
+}
 
-    header("Location: listarServicos.php");
-?>
+header("Location: listarServicos.php");
